a10682c34fb381513c4824217eb62ce1994b513f
t.brockmeyer <t.brockmeyer@criteo.com>
2021-04-11 17:12:39 +0200

    Added STATIC_ROOT path to static files

--- bookquistproject/bookquistproject/settings.py
+++ bookquistproject/bookquistproject/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#ASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'bookquistproject','staticfiles')
+
+print("######################################")
+print(STATIC_ROOT)
+print("######################################")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
