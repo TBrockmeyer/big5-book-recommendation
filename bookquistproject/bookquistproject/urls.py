ee6a5c181cbee79f1f837ca540a3b0d105ad9949
t.brockmeyer <t.brockmeyer@criteo.com>
2021-04-11 23:40:28 +0200

    Shortened app URL

--- bookquistproject/bookquistproject/urls.py
+++ bookquistproject/bookquistproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('bookquistapp/', include('bookquistapp.urls')),
+    path('app/', include('bookquistapp.urls')),
     path('admin/', admin.site.urls),
 ]
