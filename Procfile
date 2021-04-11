d8e89685e820cdbb18f080921beb93b8bf53469a
t.brockmeyer <t.brockmeyer@criteo.com>
2021-04-11 16:31:32 +0200

    Initial Django commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir ./bookquistproject bookquistproject.wsgi --log-file -
---
+++ bookquistproject/bookquistproject/urls.py
@@ -0,0 +1,22 @@
+"""bookquistproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('bookquistapp/', include('bookquistapp.urls')),
+    path('admin/', admin.site.urls),
+]
