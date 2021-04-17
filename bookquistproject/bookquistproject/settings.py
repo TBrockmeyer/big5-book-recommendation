8dc051c563947ffb2eb7d157b6f26b5d1f88a38e
t.brockmeyer <t.brockmeyer@criteo.com>
2021-04-17 12:31:09 +0200

    Implemented whitenoise for deploying static assets with Django in production, as specified in Heroku documentation/Django and Static Assets/Whitenoise

--- bookquistproject/bookquistproject/settings.py
+++ bookquistproject/bookquistproject/settings.py
@@ -51,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bookquistproject.urls'
