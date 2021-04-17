d201d5439844ebf069f9c0cc4f188462502d5e2b
t.brockmeyer <t.brockmeyer@criteo.com>
2021-04-17 12:50:13 +0200

    Added slash to default URL patterns

--- bookquistproject/bookquistapp/urls.py
+++ bookquistproject/bookquistapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employeelist', views.EmployeeListView.as_view(), name='employee_list'),
-    path('employeedetail/<int:pk>', views.EmployeeDetailView.as_view(), name='employee_detail'),
+    path('employeelist/', views.EmployeeListView.as_view(), name='employee_list'),
+    path('employeedetail/<int:pk>/', views.EmployeeDetailView.as_view(), name='employee_detail'),
 ]
