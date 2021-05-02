069147f9e78e2bded9099305d329af3b2a88b5cc
t.brockmeyer <t.brockmeyer@criteo.com>
2021-05-02 14:30:56 +0200

    Added base functionality: book recommendation

--- bookquistproject/bookquistapp/urls.py
+++ bookquistproject/bookquistapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employeelist/', views.EmployeeListView.as_view(), name='employee_list'),
-    path('employeedetail/<int:pk>/', views.EmployeeDetailView.as_view(), name='employee_detail'),
+    path('employeelist/', views.EmployeeListView.as_view(template_name="./bookquistapp/employee_list.html"), name='employee_list'),
+    path('employeedetail/<int:pk>/', views.EmployeeDetailView.as_view(template_name="./bookquistapp/employee_detail.html"), name='employee_detail'),
+    path('employeedetail/<int:pk>/bookreco/', views.EmployeeBookrecoView.as_view(template_name="./bookquistapp/employee_bookreco.html"), name='employee_bookreco'),
 ]
--- bookquistproject/bookquistapp/views.py
+++ bookquistproject/bookquistapp/views.py
@@ -30,3 +30,15 @@ class EmployeeDetailView(DetailView):
             employee_list.append({"pk": str(eo.pk), "employee_id": str(eo.employee_id), "employee_firstname": eo.employee_firstname, "employee_lastname": eo.employee_lastname, "employee_selfperception_text": eo.employee_selfperception_text})
         context["employee_list"] = employee_list
         return context
+
+class EmployeeBookrecoView(DetailView):
+    model = Employee
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        employee_list = []
+        employee_objects = Employee.objects.all().filter(employee_id=self.kwargs["pk"])
+        for eo in employee_objects:
+            employee_list.append({"pk": str(eo.pk), "employee_id": str(eo.employee_id), "employee_firstname": eo.employee_firstname, "employee_lastname": eo.employee_lastname, "employee_selfperception_text": eo.employee_selfperception_text})
+        context["employee_list"] = employee_list
+        return context
