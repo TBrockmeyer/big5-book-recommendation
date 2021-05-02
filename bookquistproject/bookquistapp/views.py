90f91146f5d73fdbe827b248483b24c63c210143
t.brockmeyer <t.brockmeyer@criteo.com>
2021-05-02 12:26:22 +0200

    Switched also single-employee view and template to array object representation

--- bookquistproject/bookquistapp/views.py
+++ bookquistproject/bookquistapp/views.py
@@ -24,5 +24,9 @@ class EmployeeDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['employee_details'] = Employee.objects.get(employee_id=self.kwargs["pk"])
+        employee_list = []
+        employee_objects = Employee.objects.all().filter(employee_id=self.kwargs["pk"])
+        for eo in employee_objects:
+            employee_list.append({"pk": str(eo.pk), "employee_id": str(eo.employee_id), "employee_firstname": eo.employee_firstname, "employee_lastname": eo.employee_lastname, "employee_selfperception_text": eo.employee_selfperception_text})
+        context["employee_list"] = employee_list
         return context
